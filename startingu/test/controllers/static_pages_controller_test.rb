ce82c5e165591023d96fde6532b7301beec4cdcd
jstarr326 <jstarrtaylor@gmail.com>
2016-01-05 17:40:30 -0800

    Finished home and about static pages

--- startingu/test/controllers/static_pages_controller_test.rb
+++ startingu/test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,14 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | StartingU"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | StartingU"
+
   end
 
 end
